ebabb48e9c81d8436e960b5e866b7399cf2e1ead
nenjotsu <nenjotsu@protonmail.com>
2025-06-29 21:04:44 +0800

    add renderer

---
+++ renderer/input.go
@@ -0,0 +1,108 @@
+package renderer
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/nenjotsu/microui-go"
+)
+
+// repeatingKeyPressed return true when key is pressed considering the repeat state.
+func repeatingKeyPressed(key ebiten.Key) bool {
+	const (
+		delay    = 40
+		interval = 4
+	)
+	d := inpututil.KeyPressDuration(key)
+	if d == 1 {
+		return true
+	}
+	if d >= delay && (d-delay)%interval == 0 {
+		return true
+	}
+	return false
+}
+
+// updates the input state of the context
+func (mgr *Manager) Update() {
+	ctx := mgr.Ctx
+
+	// mouse movement
+	cx, cy := ebiten.CursorPosition()
+	if cx != mgr.cx || cy != mgr.cy {
+		ctx.InputMouseMove(cx, cy)
+		mgr.cx, mgr.cy = cx, cy
+	}
+
+	// scrollwheel
+	wx, wy := ebiten.Wheel()
+	if wx != 0 || wy != 0 {
+		ctx.InputScroll(int(wx*-30), int(wy*-30))
+	}
+
+	// keyboard input
+	chars := ebiten.AppendInputChars(nil)
+	if len(chars) > 0 {
+		ctx.InputText(chars)
+	}
+
+	// mouse down
+	var buttonsDown int
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+		buttonsDown |= microui.MU_MOUSE_LEFT
+	}
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonMiddle) {
+		buttonsDown |= microui.MU_MOUSE_MIDDLE
+	}
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonRight) {
+		buttonsDown |= microui.MU_MOUSE_RIGHT
+	}
+	if buttonsDown != 0 {
+		ctx.InputMouseDown(cx, cy, buttonsDown)
+	}
+
+	// mouse up
+	var buttonsUp int
+	if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
+		buttonsUp |= microui.MU_MOUSE_LEFT
+	}
+	if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonMiddle) {
+		buttonsUp |= microui.MU_MOUSE_MIDDLE
+	}
+	if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonRight) {
+		buttonsUp |= microui.MU_MOUSE_RIGHT
+	}
+	if buttonsUp != 0 {
+		ctx.InputMouseUp(cx, cy, buttonsUp)
+	}
+
+	// modifiers
+	if inpututil.IsKeyJustPressed(ebiten.KeyAlt) {
+		ctx.InputKeyDown(microui.MU_KEY_ALT)
+	} else if inpututil.IsKeyJustReleased(ebiten.KeyAlt) {
+		ctx.InputKeyUp(microui.MU_KEY_ALT)
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyControl) {
+		ctx.InputKeyDown(microui.MU_KEY_CTRL)
+	} else if inpututil.IsKeyJustReleased(ebiten.KeyControl) {
+		ctx.InputKeyUp(microui.MU_KEY_CTRL)
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
+		ctx.InputKeyDown(microui.MU_KEY_RETURN)
+	} else if inpututil.IsKeyJustReleased(ebiten.KeyEnter) {
+		ctx.InputKeyUp(microui.MU_KEY_RETURN)
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyShift) {
+		ctx.InputKeyDown(microui.MU_KEY_SHIFT)
+	} else if inpututil.IsKeyJustReleased(ebiten.KeyShift) {
+		ctx.InputKeyUp(microui.MU_KEY_SHIFT)
+	}
+
+	// repeating keys
+	if repeatingKeyPressed(ebiten.KeyBackspace) {
+		ctx.InputKeyDown(microui.MU_KEY_BACKSPACE)
+		mgr.isHoldingBackspace = true
+	} else if mgr.isHoldingBackspace {
+		ctx.InputKeyUp(microui.MU_KEY_BACKSPACE)
+		mgr.isHoldingBackspace = false
+	}
+}
---
+++ renderer/renderer.go
@@ -0,0 +1,116 @@
+package renderer
+
+import (
+	"image"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+	"github.com/nenjotsu/microui-go"
+	"github.com/nenjotsu/microui-go/icons"
+)
+
+type Manager struct {
+	Ctx                *microui.Context // microui context
+	Face               text.Face        // font face
+	FaceHeight         int
+	ClipMask           bool // whether to use clip mask
+	cx                 int
+	cy                 int
+	isHoldingBackspace bool
+}
+
+func NewManagerWithContext(ctx *microui.Context, face text.Face, faceHeight int) *Manager {
+	mgr := &Manager{
+		Ctx:        ctx,
+		Face:       face,
+		FaceHeight: faceHeight,
+		ClipMask:   true,
+	}
+	ctx.TextWidth = mgr.textWidth
+	ctx.TextHeight = mgr.textHeight
+	return mgr
+}
+
+func NewManager(face text.Face, faceHeight int) *Manager {
+	ctx := microui.NewContext()
+	return NewManagerWithContext(ctx, face, faceHeight)
+}
+
+func (mgr *Manager) textWidth(fnt microui.Font, str string) int {
+	w, _ := text.Measure(str, mgr.Face, 0.0)
+	return int(w)
+}
+
+func (mgr *Manager) textHeight(fnt microui.Font) int {
+	return mgr.FaceHeight
+}
+
+func (mgr *Manager) Draw(screen *ebiten.Image) {
+	ctx := mgr.Ctx
+	target := screen
+	ctx.Render(func(cmd *microui.Command) {
+		mgr.renderCommand(cmd, &target, screen)
+	})
+}
+
+func (mgr *Manager) renderCommand(cmd *microui.Command, target **ebiten.Image, origScreen *ebiten.Image) {
+	screen := *target
+	switch cmd.Type {
+	case microui.MU_COMMAND_CLIP:
+		if !mgr.ClipMask {
+			return
+		}
+		if cmd.Clip.Rect == microui.UnclippedRect {
+			*target = origScreen
+			return
+		}
+		*target = screen.SubImage(image.Rect(
+			cmd.Clip.Rect.X,
+			cmd.Clip.Rect.Y,
+			min(cmd.Clip.Rect.X+cmd.Clip.Rect.W, screen.Bounds().Dx()),
+			min(cmd.Clip.Rect.Y+cmd.Clip.Rect.H, screen.Bounds().Dy()),
+		)).(*ebiten.Image)
+	case microui.MU_COMMAND_RECT:
+		mgr.renderRect(cmd.Rect, screen)
+	case microui.MU_COMMAND_TEXT:
+		mgr.renderText(cmd.Text, screen)
+	case microui.MU_COMMAND_ICON:
+		mgr.renderIcon(cmd.Icon, screen)
+	}
+}
+
+func (mgr *Manager) renderRect(cmd microui.RectCommand, screen *ebiten.Image) {
+	vector.DrawFilledRect(
+		screen,
+		float32(cmd.Rect.X),
+		float32(cmd.Rect.Y),
+		float32(cmd.Rect.W),
+		float32(cmd.Rect.H),
+		cmd.Color.ToRGBA(),
+		false,
+	)
+}
+
+func (mgr *Manager) renderText(cmd microui.TextCommand, screen *ebiten.Image) {
+	x := cmd.Pos.X
+	y := cmd.Pos.Y
+
+	op := &text.DrawOptions{}
+	op.GeoM.Translate(float64(x), float64(y))
+	op.ColorScale.ScaleWithColor(cmd.Color.ToRGBA())
+
+	text.Draw(screen, cmd.Str, mgr.Face, op)
+}
+
+func (mgr *Manager) renderIcon(cmd microui.IconCommand, screen *ebiten.Image) {
+	icon := icons.DefaultIcons[cmd.Id]
+
+	x := cmd.Rect.X + (cmd.Rect.W-icon.Bounds().Dx())/2
+	y := cmd.Rect.Y + (cmd.Rect.H-icon.Bounds().Dy())/2
+
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(float64(x), float64(y))
+	screen.DrawImage(icon, op)
+
+}
